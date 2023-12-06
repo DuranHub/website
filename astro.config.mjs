e462572f7c92e582f9e5afa4a33e8cd6f12fd304
enZane <ezequiel@octahedroid.com>
2023-12-06 01:21:22 -0600

    :sparkles: first commit

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [storyblok({
+    bridge: process.env.PUBLIC_ENV === 'preview' || process.env.PUBLIC_ENV === 'development',
+    accessToken: env.STORYBLOK_TOKEN,
+    enableFallbackComponent: true,
+    components: {
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      page: 'storyblok/Page',
+      title: 'storyblok/Title',
+      hero: 'storyblok/Hero',
+    },
+    apiOptions: {
+      region: 'us'
+    }
+  }), tailwind()],
+  output: process.env.PUBLIC_ENV === 'preview' ? 'server' : 'static',
+  adapter: process.env.PUBLIC_ENV === 'preview' ? vercel() : undefined,
+});
